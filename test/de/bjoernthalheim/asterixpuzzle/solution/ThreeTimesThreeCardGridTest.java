f8ca37abee84c947c8fb8dd8149415e91915b8a0
Björn Thalheim <bjoern.thalheim@gmx.de>
2012-12-23 21:00:38 +0100

    Added Test case for full grid isomorphy.

--- test/de/bjoernthalheim/asterixpuzzle/solution/ThreeTimesThreeCardGridTest.java
+++ test/de/bjoernthalheim/asterixpuzzle/solution/ThreeTimesThreeCardGridTest.java
@@ -37,5 +37,40 @@ public class ThreeTimesThreeCardGridTest {
 		grid2.putOntoNextFreePositionSuccessful(cornerCard, Orientation.EAST);
 		assertTrue(grid1.isIsomorphic(grid2));
 	}
+	
+	@Test
+	public void testFindIsomorphicGridComplex() {
+		// Grid 1: 
+		// [MTLTABOB, ATOTABLB, MTATMBOB ]
+		// [ATLTMBOB, ATLTMBLB, MTOTABLB ]
+		// [MTATOBLB, MTOTOBAB, ATMTLBOB ]
+		ThreeTimesThreeCardGrid grid1 = new ThreeTimesThreeCardGrid();
+		assertTrue(grid1.putOntoNextFreePositionSuccessful(new CardImpl("MTLTABOB"), Orientation.NORTH));
+		assertTrue(grid1.putOntoNextFreePositionSuccessful(new CardImpl("ATOTABLB"), Orientation.NORTH));
+		assertTrue(grid1.putOntoNextFreePositionSuccessful(new CardImpl("MTATMBOB"), Orientation.NORTH));
+		assertTrue(grid1.putOntoNextFreePositionSuccessful(new CardImpl("ATLTMBOB"), Orientation.NORTH));
+		assertTrue(grid1.putOntoNextFreePositionSuccessful(new CardImpl("ATLTMBLB"), Orientation.NORTH));
+		assertTrue(grid1.putOntoNextFreePositionSuccessful(new CardImpl("MTOTABLB"), Orientation.NORTH));
+		assertTrue(grid1.putOntoNextFreePositionSuccessful(new CardImpl("MTATOBLB"), Orientation.NORTH));
+		assertTrue(grid1.putOntoNextFreePositionSuccessful(new CardImpl("MTOTOBAB"), Orientation.NORTH));
+		assertTrue(grid1.putOntoNextFreePositionSuccessful(new CardImpl("ATMTLBOB"), Orientation.NORTH));
+		// Grid 2: (= Grid 1, rotated east)
+		// [LBMTATOB, OBATLTMB, OBMTLTAB ]
+		// [ABMTOTOB, LBATLTMB, LBATOTAB ]
+		// [OBATMTLB, LBMTOTAB, OBMTATMB ]
+		ThreeTimesThreeCardGrid grid2 = new ThreeTimesThreeCardGrid();
+		assertTrue(grid2.putOntoNextFreePositionSuccessful(new CardImpl("LBMTATOB"), Orientation.NORTH));
+		assertTrue(grid2.putOntoNextFreePositionSuccessful(new CardImpl("OBATLTMB"), Orientation.NORTH));
+		assertTrue(grid2.putOntoNextFreePositionSuccessful(new CardImpl("OBMTLTAB"), Orientation.NORTH));
+		assertTrue(grid2.putOntoNextFreePositionSuccessful(new CardImpl("ABMTOTOB"), Orientation.NORTH));
+		assertTrue(grid2.putOntoNextFreePositionSuccessful(new CardImpl("LBATLTMB"), Orientation.NORTH));
+		assertTrue(grid2.putOntoNextFreePositionSuccessful(new CardImpl("LBATOTAB"), Orientation.NORTH));
+		assertTrue(grid2.putOntoNextFreePositionSuccessful(new CardImpl("OBATMTLB"), Orientation.NORTH));
+		assertTrue(grid2.putOntoNextFreePositionSuccessful(new CardImpl("LBMTOTAB"), Orientation.NORTH));
+		assertTrue(grid2.putOntoNextFreePositionSuccessful(new CardImpl("OBMTATMB"), Orientation.NORTH));
+		// Test Test on Isomophicness
+		assertTrue(grid2.isIsomorphic(grid1));
+		assertTrue(grid1.isIsomorphic(grid2));
+	}
 
 }
