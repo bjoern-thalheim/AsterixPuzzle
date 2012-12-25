2354d7046df1e911cc6c20ed2b73ab82f83d2db5
Björn Thalheim <bjoern.thalheim@gmx.de>
2012-12-25 16:21:09 +0100

    Small refactoring of the start class.

--- src/de/bjoernthalheim/asterixpuzzle/strategy/BrutForceSolutionFinderImplStarter.java
+++ src/de/bjoernthalheim/asterixpuzzle/strategy/BrutForceSolutionFinderImplStarter.java
@@ -10,10 +10,16 @@ import de.bjoernthalheim.asterixpuzzle.deck.Deck;
 import de.bjoernthalheim.asterixpuzzle.solution.DisplayableCardGrid;
 import de.bjoernthalheim.asterixpuzzle.solution.ThreeTimesThreeCardGrid;
 
+/**
+ * Simple class to start a JavaFX {@link Application} which then starts the recursion algorithm as well as the graphical
+ * representation of the result.
+ * 
+ * @author bjoern
+ */
 public class BrutForceSolutionFinderImplStarter extends Application {
 
 	/**
-	 * Recursion start. Take the full deck and an empty grid and pass that into the recursion.
+	 * Start of the JavaFX application as it is shown in the tutorials.
 	 * 
 	 * @param args
 	 *            ignored.
@@ -22,8 +28,29 @@ public class BrutForceSolutionFinderImplStarter extends Application {
 		launch(args);
 	}
 
+	/**
+	 * Recursion start. Take the full deck and an empty grid and pass that into the recursion.
+	 * 
+	 * @param args
+	 *            ignored.
+	 */
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+		List<DisplayableCardGrid> solutions = calculatePuzzleResult();
+		// Graphical output
+		for (DisplayableCardGrid solution : solutions) {
+			showSolutionGraphically(primaryStage, solution);
+		}
+	}
+
+	private void showSolutionGraphically(Stage primaryStage, DisplayableCardGrid solution) {
+		Scene scene = new SolutionJFXVisualizer(solution).createRootAndScene();
+		primaryStage.setTitle("Asterix Puzzle Solution");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+
+	private List<DisplayableCardGrid> calculatePuzzleResult() {
 		BruteForceSolutionFinderImpl impl = new BruteForceSolutionFinderImpl();
 		// init deck, grid and empty solution list.
 		Deck deck = new DisplayableDeckCreator().createNewDeck();
@@ -33,13 +60,7 @@ public class BrutForceSolutionFinderImplStarter extends Application {
 		impl.findAllSolutions(allSolutions, deck, grid);
 		// find really distinct solutions
 		List<DisplayableCardGrid> solutions = impl.removeIsomorphicSolutions(allSolutions);
-		// Graphical output
-		for (DisplayableCardGrid solution : solutions) {
-			Scene scene = new SolutionJFXVisualizer(solution).createRootAndScene();
-			primaryStage.setTitle("Asterix Puzzle Solution");
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		}
+		return solutions;
 	}
 
 }
